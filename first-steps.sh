da894386af322e0538dac27653dbc38295dc5a5f
LeopardWatt <leopard.watt@gmail.com>
2023-03-21 12:40:34 +0200

    v3

--- first-steps.sh
+++ first-steps.sh
@@ -9,26 +9,33 @@ sudo apt install mc htop ncdu mcedit git -y
 echo "Apps installed"
 # Installing Docker environment 
 # step 1
+sudo apt update \
 sudo apt install \
 ca-certificates \
 curl \
 gnupg \
 lsb-release
+echo "step 1"
 #step 2
-sudo mkdir -m 0755 -p /etc/apt/keyrings -y
+sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "step 2"
 #step 3
 echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "step 3"
 #step 4
 sudo apt update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
 echo "Docker installed"
+echo "step 4"
 # Installing Portainer
 # step 1
 sudo docker volume create portainer_data
+echo "step 1p"
 # step 2
 sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+echo "step 2p"
 echo "Portainer installed"
 exit
