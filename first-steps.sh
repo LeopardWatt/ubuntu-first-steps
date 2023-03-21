3a8a38650a0034ff36cfda1b7a7b9799a37d9ef5
LeopardWatt <leopard.watt@gmail.com>
2023-03-21 12:35:57 +0200

    v2

--- first-steps.sh
+++ first-steps.sh
@@ -22,7 +22,7 @@ echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 #step 4
-sudo apt update \
+sudo apt update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
 echo "Docker installed"
 # Installing Portainer
